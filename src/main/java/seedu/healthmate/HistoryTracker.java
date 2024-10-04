885b9029e46716907cb64937e0b4e2af9c9d74aa
Dri_water <jonasseet@gmail.com>
2024-10-04 18:26:03 +0800

    remove wildcard import

--- src/main/java/seedu/healthmate/HistoryTracker.java
+++ src/main/java/seedu/healthmate/HistoryTracker.java
@@ -1,6 +1,11 @@
 package seedu.healthmate;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
